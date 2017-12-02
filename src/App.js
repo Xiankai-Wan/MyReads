d5f71544ddbc16aaf986172bfb20412d4464a3f4
Kevin-Wan <xiankai.wan@gmail.com>
2017-12-02 11:03:39 +0800

    Add: use SearchBooks component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import { Link } from 'react-router-dom'
 import { Route } from 'react-router-dom'
+import SearchBooks from './SearchBooks'
 import './App.css'
 
 class BooksApp extends React.Component {
@@ -13,33 +14,11 @@ class BooksApp extends React.Component {
      * pages, as well as provide a good URL they can bookmark and share.
      */
   }
-
   render() {
     return (
       <div className="app">
-      <Route path = '/add' render = {()=>(
-          <div className="search-books">
-            <div className="search-books-bar">
-              <Link className="close-search" to = '/' >Close</Link>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-      )}/>
-      <Route exact path = '/' render = {()=>(
+      <Route path='/search' component={SearchBooks}/>
+      <Route exact path='/' render={()=>(
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -197,7 +176,7 @@ class BooksApp extends React.Component {
             </div>
             <div className="open-search">
               <Link
-              to = '/add'
+              to='/search'
               >Add a book</Link>
             </div>
           </div>
